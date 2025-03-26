7129ff4755ab0977bb0b014473646b717064b390
aresdevv <giovaniarias06@gmail.com>
2025-03-26 00:46:11 -0500

    probando-4

--- 02-operadores/01-aritemticos.js
+++ 02-operadores/01-aritemticos.js
@@ -1,12 +1,20 @@
 let a = 5;
 let b = 7;
 
-console.log(a + b, "adicion");
-console.log(a - b, "sustraccion");
-console.log(a * b);
-console.log(a / b);
-console.log(a % b); //residuo de la division
-console.log(a ** b); //potencia
-
+// console.log(a + b, "adicion");
+// console.log(a - b, "sustraccion");
+// console.log(a * b, "multiplicacion");
+// console.log(a / b, "division");
+// console.log(a % b); //residuo de la division
+// console.log(a ** b); //potencia
+//
 console.log("--------------");
 //fgafasfasfa
+//incrementar
+console.log(++a);
+console.log(a++);
+console.log(a);
+//decrementar
+console.log(--a);
+console.log(a--);
+console.log(a);

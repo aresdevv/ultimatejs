59348e1a765522e3508cfe850aa05edb4dc74670
ares <giovaniarias06@gmail.com>
2025-04-10 18:33:33 -0500

    operadores/5 y 7

--- 02-operadores/05-falsy.js
+++ 02-operadores/05-falsy.js
@@ -1,2 +1,21 @@
-// hola
-// hola2
+//short-circuit
+
+//falso
+//falso
+//0
+//
+let nombre = "ares";
+let username = nombre || "Anonimo";
+
+console.log(username);
+
+function fn1() {
+  console.log("soy funcion 1");
+  return false;
+}
+function fn2() {
+  console.log("soy funcion 2");
+  return true;
+}
+
+let x = fn1() && fn2();

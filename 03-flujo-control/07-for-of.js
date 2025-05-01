27062e64acd7ef5a55b0f43f7e7d528b7a463bc6
ares <giovaniarias06@gmail.com>
2025-05-01 16:04:25 -0500

    hasta el 8

---
+++ 03-flujo-control/07-for-of.js
@@ -0,0 +1,10 @@
+let alumnos = ["yazid", "diego", "deyvi", "giovanni"];
+
+for (let alumno of alumnos) {
+  console.log(alumno);
+}
+// let i = 0;
+// while (i < alumnos.length) {
+//   console.log(alumnos[i]);
+//   i++;
+// }
---
+++ 03-flujo-control/08-for-in.js
@@ -0,0 +1,9 @@
+let user = {
+  id: 1,
+  name: "giovanni",
+  age: 22,
+};
+
+for (let key in user) {
+  console.log(key, user[key]);
+}
